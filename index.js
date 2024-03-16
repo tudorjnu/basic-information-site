e22e5ff0ddee95dda94314b24324b9ebbac4b7c9
Tudor Jianu <jianu.tudor9@gmail.com>
2024-03-16 07:37:21 +0000

    make it functional

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 import { createServer } from "node:http";
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "url";
 
 const hostname = "127.0.0.1";
 const port = 3001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(200, {});
+    fs.readFile(path.join(__dirname, "public", "index.html"), (_, content) => {
+      res.writeHead(200, { ContentType: "text/html" });
+      res.end(content);
+    });
+  } else if (req.url === "/about") {
+    fs.readFile(path.join(__dirname, "public", "about.html"), (_, content) => {
+      res.writeHead(200, { ContentType: "text/html" });
+      res.end(content);
+    });
+  } else if (req.url === "/contact-me") {
+    fs.readFile(
+      path.join(__dirname, "public", "contact-me.html"),
+      (_, content) => {
+        res.writeHead(200, { ContentType: "text/html" });
+        res.end(content);
+      },
+    );
+  } else {
+    fs.readFile(path.join(__dirname, "public", "404.html"), (_, content) => {
+      res.writeHead(200, { ContentType: "text/html" });
+      res.end(content);
+    });
   }
-
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World\n");
 });
 
 server.listen(port, hostname, () => {
